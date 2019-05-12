d30f9a30eb6387d9034f52ae87d3e022df0c2b36
Christian Schaefer <schaepp@gmx.de>
2019-05-12 12:45:09 +0200

    + Added webmentions

--- .eleventy.js
+++ .eleventy.js
@@ -1,93 +1,118 @@
-const { DateTime } = require("luxon");
+const {DateTime} = require("luxon");
 const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.setDataDeepMerge(true);
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(pluginSyntaxHighlight);
+    eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+    eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-  });
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+    });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    });
 
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
-    }
+    // Get the first `n` elements of a collection.
+    eleventyConfig.addFilter("head", (array, n) => {
+        if (n < 0) {
+            return array.slice(n);
+        }
 
-    return array.slice(0, n);
-  });
+        return array.slice(0, n);
+    });
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+    // Webmentions Filter
+    eleventyConfig.addFilter('webmentionsForUrl', (webmentions, url) => {
+        const allowedTypes = ['mention-of', 'in-reply-to']
+        const allowedHTML = {
+            allowedTags: ['b', 'i', 'em', 'strong', 'a'],
+            allowedAttributes: {
+                a: ['href']
+            }
+        }
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+        const clean = entry => {
+            const {content} = entry
+            if (content && content['content-type'] === 'text/html') {
+                content.value = sanitizeHTML(content.value, allowedHTML)
+            }
+            return entry
+        };
 
-  /* Markdown Plugins */
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
-  let options = {
-    html: true,
-    breaks: true,
-    linkify: true
-  };
-  let opts = {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  };
+        return webmentions
+            .filter(entry => entry['wm-target'] === url)
+            .filter(entry => allowedTypes.includes(entry['wm-property']))
+            .filter(entry => !!entry.content)
+            .map(clean)
+    });
 
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
-  );
+    eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
 
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("css");
 
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    }
-  });
+    /* Markdown Plugins */
+    let markdownIt = require("markdown-it");
+    let markdownItAnchor = require("markdown-it-anchor");
+    let options = {
+        html: true,
+        breaks: true,
+        linkify: true
+    };
+    let opts = {
+        permalink: true,
+        permalinkClass: "direct-link",
+        permalinkSymbol: "#"
+    };
 
-  return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+    eleventyConfig.setLibrary("md", markdownIt(options)
+        .use(markdownItAnchor, opts)
+    );
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: "/",
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, browserSync) {
+                const content_404 = fs.readFileSync('_site/404.html');
 
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    passthroughFileCopy: true,
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
+                browserSync.addMiddleware("*", (req, res) => {
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            }
+        }
+    });
+
+    return {
+        templateFormats: [
+            "md",
+            "njk",
+            "html",
+            "liquid"
+        ],
+
+        // If your site lives in a different subdirectory, change this.
+        // Leading or trailing slashes are all normalized away, so don’t worry about it.
+        // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+        // This is only used for URLs (it does not affect your file structure)
+        pathPrefix: "/",
+
+        markdownTemplateEngine: "liquid",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        passthroughFileCopy: true,
+        dir: {
+            input: ".",
+            includes: "_includes",
+            data: "_data",
+            output: "_site"
+        }
+    };
 };
